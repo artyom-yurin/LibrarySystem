672a1792fa449c61c1803620de5a3076921bc811
Artyom <artem_yrin@mail.ru>
2018-02-24 23:09:51 +0300

    Added skeleton of server
    Signed-off-by: Artyom <artem_yrin@mail.ru>

---
+++ src/main/java/com/example/demo/indexContorller.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class indexContorller {
+
+    @GetMapping("/")
+    public ModelAndView login() {
+        return new ModelAndView("login");
+    }
+
+
+    @GetMapping("/catalog")
+    public ModelAndView catalog() {
+        return new ModelAndView("catalog");
+    }
+
+    @GetMapping("/control")
+    public ModelAndView control() {
+        return new ModelAndView("control");
+    }
+
+    @GetMapping("/myBooks")
+    public ModelAndView myBooks() {
+        return new ModelAndView("myBooks");
+    }
+}
