b32a075496622b3929d12a6f9727eedeea7758c0
Artyom <artem-yrin@mail.ru>
2018-03-04 20:27:18 +0300

    Added tag system for future

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.TagRepository;
 import com.example.demo.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +12,14 @@ public class IndexController {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final TagRepository tagRepository;
 
-    public IndexController(UserRepository userRepository, RoleRepository roleRepository) {
+    public IndexController(UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           TagRepository tagRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.tagRepository = tagRepository;
     }
 
     @GetMapping("/")
