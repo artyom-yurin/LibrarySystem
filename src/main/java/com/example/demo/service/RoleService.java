0ac2367ff06a7b5a4fc9167e34037095ebd3a07a
Artyom <artem-yrin@mail.ru>
2018-03-04 16:54:33 +0300

    Added TypeDocument Service Fixed find role in RoleService

--- src/main/java/com/example/demo/service/RoleService.java
+++ src/main/java/com/example/demo/service/RoleService.java
@@ -2,7 +2,6 @@ package com.example.demo.service;
 
 
 import com.example.demo.entity.user.Role;
-import com.example.demo.exception.RoleNotFoundException;
 import com.example.demo.repository.RoleRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,27 +13,7 @@ public class RoleService {
         this.roleRepository = roleRepository;
     }
 
-    public Role findById(Integer id) {
-        return roleRepository.findById(id);
-    }
-
-    public Integer getIdByName(String name)
-    {
-        Integer id;
-        name = name.toLowerCase();
-        switch (name) {
-            case "admin":
-                id = 1;
-                break;
-            case "patron":
-                id = 2;
-                break;
-            case "faculty":
-                id = 3;
-                break;
-            default:
-                throw new RoleNotFoundException();
-        }
-        return id;
+    public Role findByName(String name) {
+        return roleRepository.findByName(name.toLowerCase());
     }
 }
