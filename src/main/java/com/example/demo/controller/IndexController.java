33f478ddbc8e7e568099a8ad3da7e57baee69f32
artyom-yurin <artem_yrin@mail.ru>
2018-04-23 11:52:31 +0300

    Added urls for new HTML pages

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -20,19 +20,39 @@ public class IndexController {
         if (token == null) {
             return new ModelAndView("login");
         }
-        else if("admin".equals(token.role)){
+        else if("librarian".equals(token.role)){
             return new ModelAndView("books");
+        }
+        else if("admin".equals(token.role))
+        {
+            return new ModelAndView("librarians");
         }
         else {
             return new ModelAndView("catalog");
         }
     }
 
+    @GetMapping("/admin/librarians")
+    public ModelAndView getLibrarians(HttpServletRequest request) {
+        ParserToken token = TokenAuthenticationService.getAuthentication(request);
+        if (token == null) throw new UnauthorizedException();
+        if (!token.role.equals("admin")) throw new AccessDeniedException();
+        return new ModelAndView("librarians");
+    }
+
+    @GetMapping("/admin/log")
+    public ModelAndView getLogs(HttpServletRequest request) {
+        ParserToken token = TokenAuthenticationService.getAuthentication(request);
+        if (token == null) throw new UnauthorizedException();
+        if (!token.role.equals("admin")) throw new AccessDeniedException();
+        return new ModelAndView("loging");
+    }
+
     @GetMapping("/catalog")
     public ModelAndView catalog(HttpServletRequest request) {
         ParserToken token = TokenAuthenticationService.getAuthentication(request);
         if (token == null) throw new UnauthorizedException();
-        if (token.role.equals("admin")) throw new AccessDeniedException();
+        if (!(token.role.equals("patron") || token.role.equals("faculty") || token.role.equals("vp"))) throw new AccessDeniedException();
         return new ModelAndView("catalog");
     }
 
@@ -40,39 +60,39 @@ public class IndexController {
     public ModelAndView myBooks(HttpServletRequest request) {
         ParserToken token = TokenAuthenticationService.getAuthentication(request);
         if (token == null) throw new UnauthorizedException();
-        if (token.role.equals("admin")) throw new AccessDeniedException();
+        if (!(token.role.equals("patron") || token.role.equals("faculty") || token.role.equals("vp"))) throw new AccessDeniedException();
         return new ModelAndView("mybooks");
     }
 
-    @GetMapping("/admin/documents")
+    @GetMapping("/librarian/documents")
     public ModelAndView allDocuments(HttpServletRequest request) {
         ParserToken token = TokenAuthenticationService.getAuthentication(request);
         if (token == null) throw new UnauthorizedException();
-        if (!token.role.equals("admin") && !token.role.equals("librarian")) throw new AccessDeniedException();
+        if (!token.role.equals("librarian")) throw new AccessDeniedException();
         return new ModelAndView("books");
     }
 
-    @GetMapping("/admin/users")
+    @GetMapping("/librarian/users")
     public ModelAndView allUsers(HttpServletRequest request) {
         ParserToken token = TokenAuthenticationService.getAuthentication(request);
         if (token == null) throw new UnauthorizedException();
-        if (!token.role.equals("admin") && !token.role.equals("librarian")) throw new AccessDeniedException();
+        if (!token.role.equals("librarian")) throw new AccessDeniedException();
         return new ModelAndView("users");
     }
 
-    @GetMapping("/admin/requests")
+    @GetMapping("/librarian/requests")
     public ModelAndView allRequests(HttpServletRequest request) {
         ParserToken token = TokenAuthenticationService.getAuthentication(request);
         if (token == null) throw new UnauthorizedException();
-        if (!token.role.equals("admin") && !token.role.equals("librarian")) throw new AccessDeniedException();
+        if (!token.role.equals("librarian")) throw new AccessDeniedException();
         return new ModelAndView("request");
     }
 
-    @GetMapping("/admin/confirmation")
+    @GetMapping("/librarian/confirmation")
     public ModelAndView allConfirmation(HttpServletRequest request) {
         ParserToken token = TokenAuthenticationService.getAuthentication(request);
         if (token == null) throw new UnauthorizedException();
-        if (!token.role.equals("admin") && !token.role.equals("librarian")) throw new AccessDeniedException();
+        if (!token.role.equals("librarian")) throw new AccessDeniedException();
         return new ModelAndView("confirmation");
     }
 
@@ -80,7 +100,9 @@ public class IndexController {
     public ModelAndView allAvailable(HttpServletRequest request) {
         ParserToken token = TokenAuthenticationService.getAuthentication(request);
         if (token == null) throw new UnauthorizedException();
-        if (token.role.equals("admin")) throw new AccessDeniedException();
+        if (!(token.role.equals("patron") || token.role.equals("faculty") || token.role.equals("vp"))) throw new AccessDeniedException();
         return new ModelAndView("notifications");
     }
+
+
 }
