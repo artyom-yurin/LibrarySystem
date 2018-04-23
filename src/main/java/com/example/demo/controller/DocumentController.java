ee4c84478804599edff2e9a4fc6706419133baa5
artyom-yurin <artem_yrin@mail.ru>
2018-04-23 12:43:34 +0300

    Fixed addition copies

--- src/main/java/com/example/demo/controller/DocumentController.java
+++ src/main/java/com/example/demo/controller/DocumentController.java
@@ -92,10 +92,22 @@ public class DocumentController {
         } else {
             publisher = publisherService.findByPublisherName(documentModel.getPublisher().getPublisherName());
         }
-        Document document = new Document(documentModel.getTitle(), authors, documentModel.getPrice(), documentService.findById(documentModel.getId()).getCount(), documentModel.getTags(), publisher, documentModel.getEdition(), documentModel.isBestseller(), documentModel.isReference(), documentModel.getPublishingDate(), documentModel.getEditor(), documentModel.getType());
+
+        int count = documentService.findById(documentModel.getId()).getCount();
+        boolean needAllocation = false;
+        if (Privileges.Privilege.Priv3.compareTo(Privileges.convertStringToPrivelege(token.position)) <= 0)
+        {
+            count = documentModel.getCount();
+            if (count < 0)
+            {
+                throw new InvalidCountException();
+            }
+            needAllocation = true;
+        }
+        Document document = new Document(documentModel.getTitle(), authors, documentModel.getPrice(), count, documentModel.getTags(), publisher, documentModel.getEdition(), documentModel.isBestseller(), documentModel.isReference(), documentModel.getPublishingDate(), documentModel.getEditor(), documentModel.getType());
         document.setId(documentModel.getId());
         this.documentService.save(document);
-        bookingController.queueAllocation(document.getId());
+        if (needAllocation) bookingController.queueAllocation(document.getId());
 
         logService.newLog(token.id, "Updated document id " + documentModel.getId());
     }
@@ -119,33 +131,6 @@ public class DocumentController {
         this.documentService.remove(id);
 
         logService.newLog(token.id, "Removed document" + document.getTitle());
-    }
-
-    @PutMapping("/document/copy")
-    public void updateCopies(@RequestParam(name = "id", defaultValue = "-1") Integer documentId,
-                             @RequestParam(name = "copies", defaultValue = "-1") Integer copyCount,
-                             HttpServletRequest request)
-    {
-        ParserToken token = TokenAuthenticationService.getAuthentication(request);
-        if (token == null) throw new UnauthorizedException();
-        if (!token.role.equals("librarian")) throw new AccessDeniedException();
-        if (Privileges.Privilege.Priv3.compareTo(Privileges.convertStringToPrivelege(token.position)) > 0) throw new AccessDeniedException();
-
-        if (copyCount < 0)
-        {
-            throw new InvalidCountException();
-        }
-
-        if (documentId == -1)
-            throw new InvalidIdException();
-
-        Document document = documentService.findById(documentId);
-
-        if (document == null)
-            throw new UserNotFoundException();
-
-        document.setCount(copyCount);
-        documentService.save(document);
     }
 
     @GetMapping("/document/find")
